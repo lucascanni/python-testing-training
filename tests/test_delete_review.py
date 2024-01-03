4db6001c8208a868c7d30ae52126eaa26974c882
lucascanni <77814636+lucascanni@users.noreply.github.com>
2024-01-03 10:35:57 +0100

    test_delete

--- tests/test_delete_review.py
+++ tests/test_delete_review.py
@@ -2,13 +2,14 @@ import pytest
 
 from implementations.restaurant_reviews import RestaurantReviews
 
+# test restaurant existant
 def test_delete_existing_review():
     reviews = RestaurantReviews()
     reviews.add_review("Cafe Mocha", "Great coffee and pastries", 4)
     result = reviews.delete_review("Cafe Mocha")
     assert result == "Review deleted for Cafe Mocha."
     
-
+# test restaurant non existant
 def test_delete_non_existing_review():
     reviews = RestaurantReviews()
     with pytest.raises(ValueError):
